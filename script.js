effc549dd7122370dc8a86005dc72cf678d5b879
Aquib <mdaquibanwer@gmail.com>
2023-03-11 21:43:58 +0530

    added preview image functionality

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const filterItem = document.querySelector(".items"),
-filterImg = document.querySelectorAll(".image")
+filterImg = document.querySelectorAll(".image"),
+previewBox = document.querySelector(".preview-box"),
+closeIcon = previewBox.querySelector(".icon"),
+previewImg = previewBox.querySelector("img"),
+categoryName = previewBox.querySelector(".title p"),
+shadow = document.querySelector(".shadow")
 
 window.onload = ()=>{
     filterItem.onclick = (item)=>{
@@ -20,4 +25,22 @@ window.onload = ()=>{
             })
         }
     }
+    for(let i = 0; i<filterImg.length; i++){
+        filterImg[i].setAttribute("onclick","preview(this)");   // adding onclick attribute in all available images
+    }
+}
+
+// preview image function
+
+function preview(element){
+    let selectedPreviewImg = element.querySelector("img").src;
+    let selectedImageCategory = element.getAttribute("data-name");
+    previewImg.src = selectedPreviewImg;
+    previewBox.classList.add("show"); 
+    shadow.classList.add("show"); 
+    categoryName.innerHTML = selectedImageCategory;
+    closeIcon.addEventListener("click",()=>{
+        previewBox.classList.remove("show");
+        shadow.classList.remove("show"); 
+    })
 }
